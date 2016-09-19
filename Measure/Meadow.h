8c392a942b77da0fac607e5ad6f00caeabf05514
AniSkyWorker <holyrecycler@mail.ru>
2016-09-19 07:40:42 +0300

    добавлено второе задание "луг"

---
+++ Measure/Meadow.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class Meadow
+{
+public:
+	Meadow(int wWidth, int wHeight);
+	void Draw();
+
+private:
+	int m_wWidth;
+	int m_wHeight;
+};
+
---
+++ Measure/Window.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "DispatchEvent.h"
+#include <vector>
+#include "Meadow.h"
+
+namespace
+{
+	const int WINDOW_WIDTH = 600;
+	const int WINDOW_HEIGTH = 320;
+}
+
+class CWindow : public CAbstractInputControlWindow
+{
+public:
+    CWindow();
+
+protected:
+    // CAbstractWindow interface
+    void OnUpdateWindow(float deltaSeconds) override;
+    void OnDrawWindow(const glm::ivec2 &size) override;
+    // IInputEventAcceptor interface
+    void OnKeyDown(const SDL_KeyboardEvent &) override;
+
+private:
+    void SetupView();
+
+	Meadow m_measure;
+    glm::ivec2 m_windowSize;
+};
