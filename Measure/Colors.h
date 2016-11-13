36861eca1fffc9bc8a09b2fb4272bddf5c14b50e
AniSkyWorker <holyrecycler@mail.ru>
2016-11-14 01:01:08 +0300

    добавлен забытый файл

---
+++ Measure/Colors.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace color
+{
+	const glm::vec4 BLACK = { 0, 0, 0, 1 };
+	const glm::vec4 WHITE = { 255, 255, 255, 1 };
+	const glm::vec4 LIGHT_YELLOW = { 1.f, 1.f, 0.5f, 1.f };
+	const glm::vec4 RED = { 1, 0, 0, 1.f };
+	const glm::vec4 QUIET_RED = { 0.8f, 0, 0, 1.f };
+	const glm::vec4 ORANGE = { 1.f, 0.5f, 0.f, 1.f };
+	const glm::vec4 YELLOW = { 1.f, 1.f, 0.f, 1.f };
+	const glm::vec4 QUIET_YELLOW = { 0.8f, 0.8f, 0.f, 1.f };
+	const glm::vec4 QUIET_GREEN = { 0.f, 0.5f, 0.2f, 1.f };
+	const glm::vec4 MIDLLE_GREEN = { 0.f, 0.8f, 0.f, 1.f };
+	const glm::vec4 GREEN = { 0.f, 1.f, 0.f, 1.f };
+	const glm::vec4 BLUE = { 0.f, 0.f, 1.f, 1.f};
+	const glm::vec4 QUIET_BLUE = { 0.f, 0.f, 0.8f, 1.f };
+}
